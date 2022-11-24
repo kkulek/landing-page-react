f2e90725c95bffbb0e70e3bc292b0bfe290ce12b
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-24 13:58:34 +0100

    added needed colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,24 @@
 module.exports = {
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
-    ], theme: {
-        extend: {},
+    ],
+    theme: {
+        extend: {
+            colors: {
+                underline: {
+                    "cta": "#4BE566",
+                    "gero": "#00D17C"
+                },
+                btn: {
+                    "primary": "#E04E4E",
+                    "secondary": "#EEEEEE"
+                },
+                text: {
+                    "body": "#1F1F1F",
+                    "btn-light": "#1B352B",
+                }
+            }
+        },
     },
     plugins: [],
 }
