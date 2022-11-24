64632fc1ec83ab156671549f8fb39db215505185
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-24 15:47:11 +0100

    added Hero component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import {Navigation} from "./components/Navigation";
+import {Hero} from "./components/Hero";
 
 function App() {
     return (
         <>
             <Navigation/>
+            <main>
+                <Hero/>
+            </main>
         </>
     );
 }
